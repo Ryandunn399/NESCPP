3de1ddeffb66049a58609ea7c230fbe3ca72ec02
Ryan Dunn <ryandunn399@gmail.com>
2025-10-14 17:09:01 -0400

    Added cycle checks to compare_tests.cpp

--- tests/cpu/instructions/compare_tests.cpp
+++ tests/cpu/instructions/compare_tests.cpp
@@ -6,12 +6,13 @@ TEST_F(Cpu6502Test, CMP_Immediate)
     SetupMemory(Memory6502::kRomStart, { 0xC9, 0x50 });
     cpu.A = 0x80;
 
-    cpu.ExecuteInstruction();
+    uint8_t cycles = cpu.ExecuteInstruction();
 
     EXPECT_EQ(0x80, cpu.A);  // A should remain unchanged
     EXPECT_EQ(1, cpu.StatusReg.GetCarry());     // A >= memory
     EXPECT_EQ(0, cpu.StatusReg.GetZero());      // A != memory
     EXPECT_EQ(0, cpu.StatusReg.GetNegative());  // Result bit 7 is 0
+    EXPECT_EQ(2, cycles);
     AssertPCLocation(cpu, 2);
 }
 
@@ -21,12 +22,13 @@ TEST_F(Cpu6502Test, CMP_ZeroPage)
     SetupMemory(0x40, { 0x50 });
     cpu.A = 0x80;
 
-    cpu.ExecuteInstruction();
+    uint8_t cycles = cpu.ExecuteInstruction();
 
     EXPECT_EQ(0x80, cpu.A);  // A should remain unchanged
     EXPECT_EQ(1, cpu.StatusReg.GetCarry());     // A >= memory
     EXPECT_EQ(0, cpu.StatusReg.GetZero());      // A != memory
     EXPECT_EQ(0, cpu.StatusReg.GetNegative());  // Result bit 7 is 0
+    EXPECT_EQ(3, cycles);
     AssertPCLocation(cpu, 2);
 }
 
@@ -37,12 +39,13 @@ TEST_F(Cpu6502Test, CMP_ZeroPageX)
     SetupMemory(0x50, { 0x50 });
     cpu.A = 0x80;
 
-    cpu.ExecuteInstruction();
+    uint8_t cycles = cpu.ExecuteInstruction();
 
     EXPECT_EQ(0x80, cpu.A);  // A should remain unchanged
     EXPECT_EQ(1, cpu.StatusReg.GetCarry());     // A >= memory
     EXPECT_EQ(0, cpu.StatusReg.GetZero());      // A != memory
     EXPECT_EQ(0, cpu.StatusReg.GetNegative());  // Result bit 7 is 0
+    EXPECT_EQ(4, cycles);
     AssertPCLocation(cpu, 2);
 }
 
@@ -52,12 +55,13 @@ TEST_F(Cpu6502Test, CMP_Absolute)
     SetupMemory(0xA3C4, { 0x50 });
     cpu.A = 0x80;
 
-    cpu.ExecuteInstruction();
+    uint8_t cycles = cpu.ExecuteInstruction();
 
     EXPECT_EQ(0x80, cpu.A);  // A should remain unchanged
     EXPECT_EQ(1, cpu.StatusReg.GetCarry());     // A >= memory
     EXPECT_EQ(0, cpu.StatusReg.GetZero());      // A != memory
     EXPECT_EQ(0, cpu.StatusReg.GetNegative());  // Result bit 7 is 0
+    EXPECT_EQ(4, cycles);
     AssertPCLocation(cpu, 3);
 }
 
@@ -68,12 +72,30 @@ TEST_F(Cpu6502Test, CMP_AbsoluteX)
     SetupMemory(0xA3C4, { 0x50 });
     cpu.A = 0x80;
 
-    cpu.ExecuteInstruction();
+    uint8_t cycles = cpu.ExecuteInstruction();
 
     EXPECT_EQ(0x80, cpu.A);  // A should remain unchanged
     EXPECT_EQ(1, cpu.StatusReg.GetCarry());     // A >= memory
     EXPECT_EQ(0, cpu.StatusReg.GetZero());      // A != memory
     EXPECT_EQ(0, cpu.StatusReg.GetNegative());  // Result bit 7 is 0
+    EXPECT_EQ(4, cycles);
+    AssertPCLocation(cpu, 3);
+}
+
+TEST_F(Cpu6502Test, CMP_AbsoluteXPageCross)
+{
+    SetupMemory(Memory6502::kRomStart, { 0xDD, 0xB4, 0xA3 });
+    cpu.X = 0xFF;
+    SetupMemory(0xA3B4 + 0xFF, { 0x50 });
+    cpu.A = 0x80;
+
+    uint8_t cycles = cpu.ExecuteInstruction();
+
+    EXPECT_EQ(0x80, cpu.A);  // A should remain unchanged
+    EXPECT_EQ(1, cpu.StatusReg.GetCarry());     // A >= memory
+    EXPECT_EQ(0, cpu.StatusReg.GetZero());      // A != memory
+    EXPECT_EQ(0, cpu.StatusReg.GetNegative());  // Result bit 7 is 0
+    EXPECT_EQ(5, cycles);
     AssertPCLocation(cpu, 3);
 }
 
@@ -84,12 +106,30 @@ TEST_F(Cpu6502Test, CMP_AbsoluteY)
     SetupMemory(0xA3C4, { 0x50 });
     cpu.A = 0x80;
 
-    cpu.ExecuteInstruction();
+    uint8_t cycles = cpu.ExecuteInstruction();
 
     EXPECT_EQ(0x80, cpu.A);  // A should remain unchanged
     EXPECT_EQ(1, cpu.StatusReg.GetCarry());     // A >= memory
     EXPECT_EQ(0, cpu.StatusReg.GetZero());      // A != memory
     EXPECT_EQ(0, cpu.StatusReg.GetNegative());  // Result bit 7 is 0
+    EXPECT_EQ(4, cycles);
+    AssertPCLocation(cpu, 3);
+}
+
+TEST_F(Cpu6502Test, CMP_AbsoluteYPageCross)
+{
+    SetupMemory(Memory6502::kRomStart, { 0xD9, 0xB4, 0xA3 });
+    cpu.Y = 0xFF;
+    SetupMemory(0xA3B4 + 0xFF, { 0x50 });
+    cpu.A = 0x80;
+
+    uint8_t cycles = cpu.ExecuteInstruction();
+
+    EXPECT_EQ(0x80, cpu.A);  // A should remain unchanged
+    EXPECT_EQ(1, cpu.StatusReg.GetCarry());     // A >= memory
+    EXPECT_EQ(0, cpu.StatusReg.GetZero());      // A != memory
+    EXPECT_EQ(0, cpu.StatusReg.GetNegative());  // Result bit 7 is 0
+    EXPECT_EQ(5, cycles);
     AssertPCLocation(cpu, 3);
 }
 
@@ -101,12 +141,13 @@ TEST_F(Cpu6502Test, CMP_IndirectX)
     SetupMemory(0xA3C4, { 0x50 });
     cpu.A = 0x80;
 
-    cpu.ExecuteInstruction();
+    uint8_t cycles = cpu.ExecuteInstruction();
 
     EXPECT_EQ(0x80, cpu.A);  // A should remain unchanged
     EXPECT_EQ(1, cpu.StatusReg.GetCarry());     // A >= memory
     EXPECT_EQ(0, cpu.StatusReg.GetZero());      // A != memory
     EXPECT_EQ(0, cpu.StatusReg.GetNegative());  // Result bit 7 is 0
+    EXPECT_EQ(6, cycles);
     AssertPCLocation(cpu, 2);
 }
 
@@ -118,12 +159,31 @@ TEST_F(Cpu6502Test, CMP_IndirectY)
     SetupMemory(0xA3C4, { 0x50 });
     cpu.A = 0x80;
 
-    cpu.ExecuteInstruction();
+    uint8_t cycles = cpu.ExecuteInstruction();
 
     EXPECT_EQ(0x80, cpu.A);  // A should remain unchanged
     EXPECT_EQ(1, cpu.StatusReg.GetCarry());     // A >= memory
     EXPECT_EQ(0, cpu.StatusReg.GetZero());      // A != memory
     EXPECT_EQ(0, cpu.StatusReg.GetNegative());  // Result bit 7 is 0
+    EXPECT_EQ(5, cycles);
+    AssertPCLocation(cpu, 2);
+}
+
+TEST_F(Cpu6502Test, CMP_IndirectYPageCross)
+{
+    SetupMemory(Memory6502::kRomStart, { 0xD1, 0x40 });
+    cpu.Y = 0xFF;
+    SetupMemory(0x40, { 0xB4, 0xA3 });  // Low and high byte of address
+    SetupMemory(0xA3B4 + 0xFF, { 0x50 });
+    cpu.A = 0x80;
+
+    uint8_t cycles = cpu.ExecuteInstruction();
+
+    EXPECT_EQ(0x80, cpu.A);  // A should remain unchanged
+    EXPECT_EQ(1, cpu.StatusReg.GetCarry());     // A >= memory
+    EXPECT_EQ(0, cpu.StatusReg.GetZero());      // A != memory
+    EXPECT_EQ(0, cpu.StatusReg.GetNegative());  // Result bit 7 is 0
+    EXPECT_EQ(6, cycles);
     AssertPCLocation(cpu, 2);
 }
 
@@ -132,7 +192,7 @@ TEST_F(Cpu6502Test, CMP_Equal)
     SetupMemory(Memory6502::kRomStart, { 0xC9, 0x80 });
     cpu.A = 0x80;
 
-    cpu.ExecuteInstruction();
+    uint8_t cycles = cpu.ExecuteInstruction();
 
     EXPECT_EQ(0x80, cpu.A);  // A should remain unchanged
     EXPECT_EQ(1, cpu.StatusReg.GetCarry());     // A >= memory
@@ -146,7 +206,7 @@ TEST_F(Cpu6502Test, CMP_LessThan)
     SetupMemory(Memory6502::kRomStart, { 0xC9, 0x80 });
     cpu.A = 0x50;
 
-    cpu.ExecuteInstruction();
+    uint8_t cycles = cpu.ExecuteInstruction();
 
     EXPECT_EQ(0x50, cpu.A);  // A should remain unchanged
     EXPECT_EQ(0, cpu.StatusReg.GetCarry());     // A < memory
@@ -160,7 +220,7 @@ TEST_F(Cpu6502Test, CMP_GreaterThan)
     SetupMemory(Memory6502::kRomStart, { 0xC9, 0x30 });
     cpu.A = 0x80;
 
-    cpu.ExecuteInstruction();
+    uint8_t cycles = cpu.ExecuteInstruction();
 
     EXPECT_EQ(0x80, cpu.A);  // A should remain unchanged
     EXPECT_EQ(1, cpu.StatusReg.GetCarry());     // A >= memory
@@ -174,12 +234,13 @@ TEST_F(Cpu6502Test, CPX_Immediate)
     SetupMemory(Memory6502::kRomStart, { 0xE0, 0x50 });
     cpu.X = 0x80;
 
-    cpu.ExecuteInstruction();
+    uint8_t cycles = cpu.ExecuteInstruction();
 
     EXPECT_EQ(0x80, cpu.X);  // X should remain unchanged
     EXPECT_EQ(1, cpu.StatusReg.GetCarry());     // X >= memory
     EXPECT_EQ(0, cpu.StatusReg.GetZero());      // X != memory
     EXPECT_EQ(0, cpu.StatusReg.GetNegative());  // Result bit 7 is 0
+    EXPECT_EQ(2, cycles);
     AssertPCLocation(cpu, 2);
 }
 
@@ -189,12 +250,13 @@ TEST_F(Cpu6502Test, CPX_ZeroPage)
     SetupMemory(0x40, { 0x50 });
     cpu.X = 0x80;
 
-    cpu.ExecuteInstruction();
+    uint8_t cycles = cpu.ExecuteInstruction();
 
     EXPECT_EQ(0x80, cpu.X);  // X should remain unchanged
     EXPECT_EQ(1, cpu.StatusReg.GetCarry());     // X >= memory
     EXPECT_EQ(0, cpu.StatusReg.GetZero());      // X != memory
     EXPECT_EQ(0, cpu.StatusReg.GetNegative());  // Result bit 7 is 0
+    EXPECT_EQ(3, cycles);
     AssertPCLocation(cpu, 2);
 }
 
@@ -204,12 +266,13 @@ TEST_F(Cpu6502Test, CPX_Absolute)
     SetupMemory(0xA3C4, { 0x50 });
     cpu.X = 0x80;
 
-    cpu.ExecuteInstruction();
+    uint8_t cycles = cpu.ExecuteInstruction();
 
     EXPECT_EQ(0x80, cpu.X);  // X should remain unchanged
     EXPECT_EQ(1, cpu.StatusReg.GetCarry());     // X >= memory
     EXPECT_EQ(0, cpu.StatusReg.GetZero());      // X != memory
     EXPECT_EQ(0, cpu.StatusReg.GetNegative());  // Result bit 7 is 0
+    EXPECT_EQ(4, cycles);
     AssertPCLocation(cpu, 3);
 }
 
@@ -218,7 +281,7 @@ TEST_F(Cpu6502Test, CPX_Equal)
     SetupMemory(Memory6502::kRomStart, { 0xE0, 0x80 });
     cpu.X = 0x80;
 
-    cpu.ExecuteInstruction();
+    uint8_t cycles = cpu.ExecuteInstruction();
 
     EXPECT_EQ(0x80, cpu.X);  // X should remain unchanged
     EXPECT_EQ(1, cpu.StatusReg.GetCarry());     // X >= memory
@@ -232,7 +295,7 @@ TEST_F(Cpu6502Test, CPX_LessThan)
     SetupMemory(Memory6502::kRomStart, { 0xE0, 0x80 });
     cpu.X = 0x50;
 
-    cpu.ExecuteInstruction();
+    uint8_t cycles = cpu.ExecuteInstruction();
 
     EXPECT_EQ(0x50, cpu.X);  // X should remain unchanged
     EXPECT_EQ(0, cpu.StatusReg.GetCarry());     // X < memory
@@ -246,7 +309,7 @@ TEST_F(Cpu6502Test, CPX_GreaterThan)
     SetupMemory(Memory6502::kRomStart, { 0xE0, 0x30 });
     cpu.X = 0x80;
 
-    cpu.ExecuteInstruction();
+    uint8_t cycles = cpu.ExecuteInstruction();
 
     EXPECT_EQ(0x80, cpu.X);  // X should remain unchanged
     EXPECT_EQ(1, cpu.StatusReg.GetCarry());     // X >= memory
@@ -260,12 +323,13 @@ TEST_F(Cpu6502Test, CPY_Immediate)
     SetupMemory(Memory6502::kRomStart, { 0xC0, 0x50 });
     cpu.Y = 0x80;
 
-    cpu.ExecuteInstruction();
+    uint8_t cycles = cpu.ExecuteInstruction();
 
     EXPECT_EQ(0x80, cpu.Y);  // Y should remain unchanged
     EXPECT_EQ(1, cpu.StatusReg.GetCarry());     // Y >= memory
     EXPECT_EQ(0, cpu.StatusReg.GetZero());      // Y != memory
     EXPECT_EQ(0, cpu.StatusReg.GetNegative());  // Result bit 7 is 0
+    EXPECT_EQ(2, cycles);
     AssertPCLocation(cpu, 2);
 }
 
@@ -275,12 +339,13 @@ TEST_F(Cpu6502Test, CPY_ZeroPage)
     SetupMemory(0x40, { 0x50 });
     cpu.Y = 0x80;
 
-    cpu.ExecuteInstruction();
+    uint8_t cycles = cpu.ExecuteInstruction();
 
     EXPECT_EQ(0x80, cpu.Y);  // Y should remain unchanged
     EXPECT_EQ(1, cpu.StatusReg.GetCarry());     // Y >= memory
     EXPECT_EQ(0, cpu.StatusReg.GetZero());      // Y != memory
     EXPECT_EQ(0, cpu.StatusReg.GetNegative());  // Result bit 7 is 0
+    EXPECT_EQ(3, cycles);
     AssertPCLocation(cpu, 2);
 }
 
@@ -290,12 +355,13 @@ TEST_F(Cpu6502Test, CPY_Absolute)
     SetupMemory(0xA3C4, { 0x50 });
     cpu.Y = 0x80;
 
-    cpu.ExecuteInstruction();
+    uint8_t cycles = cpu.ExecuteInstruction();
 
     EXPECT_EQ(0x80, cpu.Y);  // Y should remain unchanged
     EXPECT_EQ(1, cpu.StatusReg.GetCarry());     // Y >= memory
     EXPECT_EQ(0, cpu.StatusReg.GetZero());      // Y != memory
     EXPECT_EQ(0, cpu.StatusReg.GetNegative());  // Result bit 7 is 0
+    EXPECT_EQ(4, cycles);
     AssertPCLocation(cpu, 3);
 }
 
@@ -304,7 +370,7 @@ TEST_F(Cpu6502Test, CPY_Equal)
     SetupMemory(Memory6502::kRomStart, { 0xC0, 0x80 });
     cpu.Y = 0x80;
 
-    cpu.ExecuteInstruction();
+    uint8_t cycles = cpu.ExecuteInstruction();
 
     EXPECT_EQ(0x80, cpu.Y);  // Y should remain unchanged
     EXPECT_EQ(1, cpu.StatusReg.GetCarry());     // Y >= memory
@@ -318,7 +384,7 @@ TEST_F(Cpu6502Test, CPY_LessThan)
     SetupMemory(Memory6502::kRomStart, { 0xC0, 0x80 });
     cpu.Y = 0x50;
 
-    cpu.ExecuteInstruction();
+    uint8_t cycles = cpu.ExecuteInstruction();
 
     EXPECT_EQ(0x50, cpu.Y);  // Y should remain unchanged
     EXPECT_EQ(0, cpu.StatusReg.GetCarry());     // Y < memory
@@ -332,7 +398,7 @@ TEST_F(Cpu6502Test, CPY_GreaterThan)
     SetupMemory(Memory6502::kRomStart, { 0xC0, 0x30 });
     cpu.Y = 0x80;
 
-    cpu.ExecuteInstruction();
+    uint8_t cycles = cpu.ExecuteInstruction();
 
     EXPECT_EQ(0x80, cpu.Y);  // Y should remain unchanged
     EXPECT_EQ(1, cpu.StatusReg.GetCarry());     // Y >= memory
